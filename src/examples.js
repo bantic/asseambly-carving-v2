1e97c4c42d62fe375e735544914b9ef6a77f9a4e
Cory <cory.forsyth@gmail.com>
2019-08-11 12:56:42 -0400

    use arrayViews instead of arrays

--- src/examples.js
+++ src/examples.js
@@ -2,13 +2,13 @@ import { ImageDisplayer } from './image-utils';
 import { allocArray } from './as-utils';
 
 export async function invert8bit(imageData, module) {
-  let pixelsRef = allocArray(
-    module,
-    new Uint8ClampedArray(imageData.data.buffer)
-  );
+  console.time('invert8bit');
+  let pixelsRef = allocArray(module, new Uint8Array(imageData.data.buffer));
   module.invertImage8(pixelsRef);
 
-  let newPixels = new Uint8ClampedArray(module.__getArray(pixelsRef));
+  let newPixels = new Uint8ClampedArray(
+    module.__getArrayView(pixelsRef).buffer
+  );
   let imageDisplayer = new ImageDisplayer(
     newPixels,
     imageData.width,
@@ -16,14 +16,16 @@ export async function invert8bit(imageData, module) {
   );
   await imageDisplayer.drawIntoCanvas();
   module.__release(pixelsRef);
+  console.timeEnd('invert8bit');
 }
 
 export async function invert32bit(imageData, module) {
+  console.time('invert32bit');
   let pixelsRef = allocArray(module, new Uint32Array(imageData.data.buffer));
   module.invertImage32(pixelsRef);
 
   let newPixels = new Uint8ClampedArray(
-    new Uint32Array(module.__getArray(pixelsRef)).buffer
+    module.__getArrayView(pixelsRef).buffer
   );
   let imageDisplayer = new ImageDisplayer(
     newPixels,
@@ -32,4 +34,5 @@ export async function invert32bit(imageData, module) {
   );
   await imageDisplayer.drawIntoCanvas();
   module.__release(pixelsRef);
+  console.timeEnd('invert32bit');
 }
