538ae2a0e0ba5db7af571fd19b258dab7b8e8e35
Cory <cory.forsyth@gmail.com>
2019-08-11 13:05:09 -0400

    test out allocating an array on the wasm side, add todo notes

--- assembly/index.ts
+++ assembly/index.ts
@@ -5,6 +5,18 @@ export const ID_Uint32Array = idof<Int32Array>();
 
 const FULL_ALPHA = 0xff000000;
 
+// This was a test to make sure that creating an array on th wasm side
+// worked ok and it could be passed back
+// NEXT Up: write the code to generate a pixel difference (left-right and top-bottom)
+// for each pixel into the energy map
+export function generateEnergyMap(pixels: Uint8Array): Uint8Array {
+  let result = new Uint8Array(pixels.length);
+  for (let i = 0; i < pixels.length; i++) {
+    result[i] = pixels[i];
+  }
+  return result;
+}
+
 export function invertImage8(pixels: Uint8Array): void {
   trace('invertImage8, # pixels: ', 1, pixels.length);
   for (let i = 0; i < pixels.length; i += 4) {
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import loader from 'assemblyscript/lib/loader';
 import './style.css';
 import { ImageLoader } from './image-utils';
 import { invert8bit, invert32bit } from './examples';
+import { allocArray } from './as-utils';
 
 const MODULE_PATH = 'wasm/untouched.wasm';
 const IMAGE_URL = 'public/images/silly.jpg';
@@ -12,6 +13,10 @@ const IMAGE_URL = 'public/images/silly.jpg';
   let imgLoader = new ImageLoader(IMAGE_URL, true);
   let imageData = await imgLoader.load();
 
-  await invert32bit(imageData, module);
-  await invert8bit(imageData, module);
+  let inputPtr = allocArray(module, imageData.data);
+  let outputPtr = module.generateEnergyMap(inputPtr);
+  debugger;
+
+  // await invert32bit(imageData, module);
+  // await invert8bit(imageData, module);
 })();
