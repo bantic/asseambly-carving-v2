8374aa4a95cc1cc56bd0e3027e65700969c71357
Cory <cory.forsyth@gmail.com>
2019-08-15 19:35:35 -0400

    WIP calculating energy map

--- assembly/index.ts
+++ assembly/index.ts
@@ -4,17 +4,93 @@ export const ID_Uint8Array = idof<Uint8Array>();
 export const ID_Uint32Array = idof<Int32Array>();
 
 const FULL_ALPHA = 0xff000000;
+const BYTES_PER_PIXEL = 4; // r, g, b and a
+const MAX_ENERGY = 255 * 255 * 4; // 255*255 per channel * 4 channels
 
 // This was a test to make sure that creating an array on th wasm side
 // worked ok and it could be passed back
 // NEXT Up: write the code to generate a pixel difference (left-right and top-bottom)
 // for each pixel into the energy map
-export function generateEnergyMap(pixels: Uint8Array): Uint8Array {
-  let result = new Uint8Array(pixels.length);
-  for (let i = 0; i < pixels.length; i++) {
-    result[i] = pixels[i];
+export function calculateEnergyMap(
+  pixels: Uint8Array,
+  width: i32,
+  height: i32
+): Uint32Array {
+  let energyMap = new Uint32Array(pixels.length / 4);
+  let maxEnergy = 0;
+  let energyIndex = 0;
+  for (let x = 0; x < width; x++) {
+    for (let y = 0; y < height; y++) {
+      let energy = calculateEnergy(pixels, x, y, width, height);
+      assert(energy >= 0, 'energy is >= 0');
+      assert(energy <= MAX_ENERGY, 'energy is within bounds');
+      if (energy > maxEnergy) {
+        trace(
+          'max energy exceeded at x,y,energyIndex,prevE,maxE',
+          4,
+          x,
+          y,
+          energyIndex,
+          energy,
+          maxEnergy
+        );
+        maxEnergy = energy;
+      }
+      energyMap[energyIndex] = energy;
+      energyIndex++;
+    }
   }
-  return result;
+  return energyMap;
+}
+
+function calculateEnergy(
+  pixels: Uint8Array,
+  x: i32,
+  y: i32,
+  width: i32,
+  height: i32
+): i32 {
+  let leftX = max(x - 1, 0);
+  let leftY = y;
+  let rightX = min(x + 1, width - 1);
+  let rightY = y;
+
+  let upY = max(y - 1, 0);
+  let upX = x;
+  let downY = min(y + 1, height - 1);
+  let downX = x;
+
+  let diff = 0;
+  diff += calculateEnergyDiff(
+    pixels,
+    getPixelIndex(leftX, leftY, width),
+    getPixelIndex(rightX, rightY, width)
+  );
+  diff += calculateEnergyDiff(
+    pixels,
+    getPixelIndex(upX, upY, width),
+    getPixelIndex(downX, downY, width)
+  );
+  return diff;
+}
+
+function calculateEnergyDiff(
+  pixels: Uint8Array,
+  leftPixelIdx: i32,
+  rightPixelIdx: i32
+): i32 {
+  let diff: i32 = 0;
+  for (let i = 0; i < 4; i++) {
+    let channelDiff: i32 = pixels[leftPixelIdx + i] - pixels[rightPixelIdx + i];
+    assert(channelDiff >= 0, 'channelDiff is >= 0');
+    assert(channelDiff <= 255, 'channelDiff is <= 255');
+    diff += channelDiff * channelDiff;
+  }
+  return diff;
+}
+
+function getPixelIndex(x: i32, y: i32, width: i32): i32 {
+  return BYTES_PER_PIXEL * (y * width + x);
 }
 
 export function invertImage8(pixels: Uint8Array): void {
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import loader from 'assemblyscript/lib/loader';
 import './style.css';
-import { ImageLoader } from './image-utils';
+import { ImageLoader, ImageDisplayer } from './image-utils';
 import { invert8bit, invert32bit } from './examples';
 import { allocArray } from './as-utils';
 
@@ -14,9 +14,56 @@ const IMAGE_URL = 'public/images/silly.jpg';
   let imageData = await imgLoader.load();
 
   let inputPtr = allocArray(module, imageData.data);
-  let outputPtr = module.generateEnergyMap(inputPtr);
-  debugger;
+  let outputPtr = module.calculateEnergyMap(
+    inputPtr,
+    imageData.width,
+    imageData.height
+  );
+
+  let energyMap = module.__getArray(outputPtr); // Uint32Array
+  // normalizeEnergyMap(energyMap);
+  fullAlphaEnergyMap(energyMap);
+  let energyMapU8 = new Uint8ClampedArray(Uint32Array.from(energyMap).buffer);
+
+  let displayer = new ImageDisplayer(
+    energyMapU8,
+    imageData.width,
+    imageData.height
+  );
+  displayer.drawIntoCanvas();
 
   // await invert32bit(imageData, module);
   // await invert8bit(imageData, module);
 })();
+
+/**
+ *
+ * @param {Uint32Array} energyMap
+ */
+function normalizeEnergyMap(energyMap) {
+  const FULL_ALPHA = 0xff000000;
+  const MAX_RGB = 0xffffff;
+  const FULL_RGBA = FULL_ALPHA | MAX_RGB;
+
+  let max = 0;
+  for (let i = 0; i < energyMap.length; i++) {
+    let energy = energyMap[i];
+    if (energy > max) {
+      max = energy;
+    }
+  }
+
+  for (let i = 0; i < energyMap.length; i++) {
+    let energy = energyMap[i] / max;
+    energyMap[i] = FULL_ALPHA | (energy * MAX_RGB);
+    // let normalizedValue = (rgbaValue / max) * FULL_RGBA;
+    // energyMap[i] = normalizedValue;
+  }
+}
+
+function fullAlphaEnergyMap(energyMap) {
+  const FULL_ALPHA = 0xff000000;
+  for (let i = 0; i < energyMap.length; i++) {
+    energyMap[i] = FULL_ALPHA | energyMap[i];
+  }
+}
